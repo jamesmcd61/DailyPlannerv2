d1ab73eb9960ca66f1c1f38a44ed8b542cb72f62
jamesmcd61 <bezzinga3@gmail.com>
2024-07-14 14:10:02 +0200

    Addition of Todo Popup message and buton click for it

--- WebApplication1/Controllers/PlannerController.cs
+++ WebApplication1/Controllers/PlannerController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Services;
 
 namespace WebApplication1.Controllers
 {
     public class PlannerController : Controller
     {
+        private readonly IPlannerService plannerService;
+
+        public PlannerController(IPlannerService plannerService)
+        {
+            this.plannerService = plannerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var todos = this.plannerService.GetPlannedTodoForToday(DateTime.Now);
+            var vm = new Planner
+            {
+                Day = DateTime.Now,
+                Todos = todos
+            };
+            return View(vm);
         }
     }
 }
